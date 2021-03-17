aadf7ad6919af4e2ba8629f88478884a0eb19b1d
Keith Crooc <keith.crooc@gmail.com>
2021-03-17 00:03:49 -0400

    API calls to google search to populate search results

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,15 +1,63 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import SearchBar from "../components/SearchBar"
 import BookResult from "../components/BookResult"
+import axios from 'axios';
+import API from '../utils/API'
+
+
 
 const Search = () => {
+
+    const [book, setBook] = useState("");
+    const [result, setResult]= useState([]);
+    const [apiKey, setApiKey] = useState("AIzaSyApeATiH12wsGB3uPjN4fcEUacX-uKqVvI")
+
+    
+    // function loadBooks() {
+    //     axios.get("https://www.googleapis.com/books/v1/volumes?q="+book+"&key="+apiKey+"&maxResults=5")
+    //     .then(data => {
+    //         setResult(data.data.items)
+    //     })
+    // }
+
+    // useEffect(() => {
+    //     loadBooks()
+    // }, [])
+    
+    function handleChange(event) {
+        const book = event.target.value
+        setBook(book)
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        // axios.get("https://www.googleapis.com/books/v1/volumes?q="+book+"&key="+apiKey+"&maxResults=5")
+        API.searchBook(book)
+        .then(data => {
+            console.log(data.data.items)
+            setResult(data.data.items)
+        })
+    }
+
     return (
         <>
-        <SearchBar />
+        <SearchBar onClick={handleSubmit} onChange={handleChange}/>
 
         <div className="container results">
         <h3>Results</h3>
-        <BookResult />
+
+        {result.map(book => {
+            console.log(book)
+            return (
+                <BookResult 
+                key={book.volumeInfo.id}
+                title={book.volumeInfo.title}
+                author={book.volumeInfo.authors}
+                description={book.volumeInfo.description}
+                image={book.volumeInfo.imageLinks ? book.volumeInfo.imageLinks.thumbnail : "https://dummyimage.com/150x200/5c666e/ced0f2&text=No+Image"}/>
+            )
+        })}
+        
         </div>
         
         
---
+++ client/src/utils/API.js
@@ -0,0 +1,29 @@
+import axios from "axios"
+
+const searchURL = "https://www.googleapis.com/books/v1/volumes?q="
+
+export default {
+    // get all the books
+    getBooks: function() {
+        return axios.get("/api/books");
+    },
+    
+    // getting just one single book
+    getBook: function(id) {
+        return axios.get("/api/books/"+id)
+    },
+
+    // deleting a book
+    deleteBook: function(id) {
+        return axios.delete("/api/books/"+id)
+    },
+
+    // saving our book and its data
+    saveBook: function(book) {
+        return axios.save("/api/books/"+book)
+    },
+
+    searchBook: function(query) {
+        return axios.get(searchURL+query)
+    }
+}
